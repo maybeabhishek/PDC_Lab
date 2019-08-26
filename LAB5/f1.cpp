b06a511e38b64cf8d9fe7aefa6c44f1f8bb70fd6
abhishek <abhishek.satapathy01@gmail.com>
2019-08-26 14:08:04 +0530

    Updateed till lab5

--- LAB5/f1.cpp
+++ LAB5/f1.cpp
@@ -4,22 +4,22 @@
 using namespace std;
 
 int main(){
-    int a[5],i;
+    int a[100],i;
     #pragma omp parallel
     {
         #pragma omp for
-        for(int i = 0; i<5; i++)
+        for(int i = 0; i<100; i++)
             a[i]=i*i;
 
         #pragma omp master
-        for(int i =0; i<5; i++)
+        for(int i =0; i<100; i++)
             printf("a[%d] = %d\n",i,a[i]);
         
         //wait
         #pragma omp barrier
 
         #pragma omp for
-        for(int i = 0; i<5; i++)
+        for(int i = 0; i<100; i++)
             a[i]+=i;
     }
 }
--- LAB5/f3.cpp
+++ LAB5/f3.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(){
     int x[10]= {0,0,0,0,0,0,0,0,0,0};
     int y[10] = {0,0,0,0,0,0,0,0,0,0};
-    #pragma omp parallel num_threads(10)
+    #pragma omp parallel num_threads(9)
     {
         int mytid = omp_get_thread_num();
         x[mytid] = x[mytid]+5;
